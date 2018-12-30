c61b83807758d00b3a9e33f8ce22e42cddccada3
Vladyslav Rechkoblit <raven73093@gmail.com>
2018-12-30 11:03:48 +0200

    Started Builder

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Layout.module.css';
+
+const layout = ( props ) => (
+    <>
+        <div>
+            Toolbar, SideDrawer, BackDrop
+        </div>
+        <main className={classes.content}>
+            { props.children }
+        </main>
+    </>
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import Burger from '../components/burger/Burger';
+
+class BurgerBuilder extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            ingredients : {
+                salad  : 0,
+                bacon  : 0,
+                cheese : 0,
+                meat   : 0
+            }
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Burger ingredients={this.state.ingredients} />
+                <div>
+                    Build Controls
+                </div>
+            </>
+        )
+    }
+}
+
+export default BurgerBuilder;
